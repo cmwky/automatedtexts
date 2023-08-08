427281e7aa7954940d94e3e60873e71c95a18509
Charles W <charlesmwhitehead@gmail.com>
2023-08-07 21:04:57 -0400

    adds project

---
+++ AutomatedTexts/Function1.cs
@@ -0,0 +1,31 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace AutomatedTexts
+{
+    public class Function1
+    {
+        [FunctionName("SendScheduledMessages")]
+        public void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            string accountSid = System.Environment.GetEnvironmentVariable("TwilioAccountSid");
+            string authToken = System.Environment.GetEnvironmentVariable("TwilioAuthToken");
+            string sender = System.Environment.GetEnvironmentVariable("TwilioSender");
+            string recipient = System.Environment.GetEnvironmentVariable("TwilioRecipient");
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "This is a text.. I mean, test.",
+                from: new Twilio.Types.PhoneNumber(sender),
+                to: new Twilio.Types.PhoneNumber(recipient)
+            );
+
+            Console.WriteLine(message.Sid);
+            log.LogInformation($"Azure function executed at: {DateTime.Now}");
+        }
+    }
+}
